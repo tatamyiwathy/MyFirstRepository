0c72afa17e9596cc892e444cb9e3315b9a26d157
tatamyiwathy <jbd00353@gmail.com>
2022-07-16 14:44:38 +0900

    What are automated tests?

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,14 @@
+import datetime
+from django.utils import timezone
 from django.db import models
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
